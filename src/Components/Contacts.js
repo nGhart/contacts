ea572ed01ce3ee8df5cd2a9b7818f5a2689ad7ea
nGhart <107068059+nGhart@users.noreply.github.com>
2023-05-17 17:34:08 +0000

    Update Contacts.js

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,49 +1,16 @@
-import React, { Component } from 'react';
-import Stack from 'react-bootstrap/Stack';
-import PP from './Avatar.jpeg';
-import Image from 'react-bootstrap/Image';
+import React from 'react';
+import SingleContact from './SingleContact';
 
-class Contacts extends Component {
-    deleteContact = (e) => {
-        e.preventDefault();
-        this.props.deleteContact(this.props.items.id);
-    }
-    render() {
-        return (
-            <>
-                {this.props.items.map((item) => (
-                    <Stack
-                        direction="horizontal"
-                        gap={3}
-                        className='now todo-row'>
-                        <Image
-                            src={PP}
-                            alt=''
-                            width={40}
-                            roundedCircle />
-                        <h6>{item.name}</h6>
-                        <h6>{item.number}</h6>
-                        <div
-                            className=" ms-auto">
-                            <h6>{item.location}</h6>
-                        </div>
-                        <div
-                            className='icons'>
-                            <span
-                                className='editIcon'>
-                                ✎
-                            </span>
-                            <span
-                                className='deleteIcon'
-                                onClick={() => this.props.deleteContact(item)}>
-                                ✖
-                            </span>
-                        </div>
-                    </Stack>
-                ))}
-            </>
-        );
-    }
+const Contacts = (props) => {
+
+    return (
+        <>
+            {props.usersData.map((user) => {
+                return <SingleContact userInfo={user} key={user.id} deleteContact={props.deleteContact} editContact={props.editContact} />
+            })}
+        </>
+    );
 }
+
 
 export default Contacts;
